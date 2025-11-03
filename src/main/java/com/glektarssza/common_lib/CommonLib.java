b6444f5627abc7772a2155decf0809a0007f35c0
G'lek Tarssza <glek@glektarssza.com>
2025-11-03 07:53:26 -0700

    Added core mod class.

---
+++ src/main/java/com/glektarssza/common_lib/CommonLib.java
@@ -0,0 +1,6 @@
+package com.glektarssza.common_lib;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.MOD_VERSION, acceptableRemoteVersions = Tags.MOD_VERSION, acceptedMinecraftVersions = "1.7.10", dependencies = Tags.MOD_DEPENDENCIES, useMetadata = true, modLanguage = "java", canBeDeactivated = false)
+public class CommonLib {}
